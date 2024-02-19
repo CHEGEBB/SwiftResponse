ced7c942d8166adc0f1c46670b5183bd98a778da
CHEGEBB <chegephil24@gmail.com>
2024-02-19 21:21:45 +0300

    update

--- js/medical.js
+++ js/medical.js
@@ -221,4 +221,34 @@
         L.marker(hospital.location).addTo(map)
             .bindPopup(hospital.name);
     });
+ // Toggle navigation
+ var menuToggle = document.querySelector('.menu-toggle');
+ menuToggle.addEventListener('click', toggleNav);
+
+ // Automatically hide navigation after a certain time of inactivity
+ var timeout;
+ function hideNav() {
+     var navList = document.getElementById("navList");
+     if (navList.classList.contains('active')) {
+         timeout = setTimeout(function() {
+             navList.classList.remove('active');
+         }, 1000); // Adjust the time (in milliseconds) as needed
+     }
+ }
+
+ // Reset timeout on user interaction
+ document.addEventListener('mousemove', function() {
+     clearTimeout(timeout);
+     hideNav();
+ });
+
+ // Initially hide navigation
+ hideNav();
+
+ function toggleNav() {
+     var navList = document.getElementById("navList");
+     navList.classList.toggle("active");
+     clearTimeout(timeout); // Clear the timeout whenever the navigation is toggled
+     hideNav(); // Restart the hideNav functionality
+ }
 });
